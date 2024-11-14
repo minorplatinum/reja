799c867278991635f4000a31d13a0546f616e07d
minorplatinum <muhammadjonfarxodov41@gmail.com>
2024-11-14 19:15:55 +0900

    feat: developauthor router ejs page

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ console.log("Web Serverni boshlash");
 const express = require("express");  // Express kutubxonasini chaqirish
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+
 
 app.use(express.static("public"));  // Statik fayllarni "public" papkasidan yuklash
 app.use(express.json());  // JSON formatidagi ma'lumotlarni ishlash
@@ -18,6 +30,11 @@ app.post("/create-item", (req, res) => {
   console.log(req.body);
   res.json({ test: "success" });
 });
+
+app.get('/author' , (req, res) => {
+  res.render("author",{ user: user});
+});
+
 
 app.get("/", function (req, res) {
   res.render("harid");  // "/gift" manzili uchun javob
